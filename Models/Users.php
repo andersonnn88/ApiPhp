2484bc4c13485f6ad1649a37988a6abeda35dce5
Anderson Araujo <andersonga4@gmail.com>
2019-11-26 21:05:45 -0300

    Criação de sistema de cadastro API

--- Models/Users.php
+++ Models/Users.php
@@ -7,6 +7,24 @@ use \Models\Jwt;
 class Users extends Model {
 
 	private $id_user;
+	
+	public function create($name, $email, $pass){
+		if(!$this->emailExists($email)){
+			$hash = password_hash($pass, PASSWORD_DEFAULT);
+			$sql = "INSERT INTO users (name, email, pass) VALUES (:name, :email, :pass)";
+			$sql = $this->db->prepare($sql);
+			$sql->bindValue(':name', $name);
+			$sql->bindValue(':email',$email);
+			$sql->bindValue(':pass', $pass);
+			$sql->execute();
+
+			$this->id_user = $this->db->lastInsertId();
+
+			return true;
+		}else{
+			return false;
+		}
+	}
 	 
 	public function checkCredentials($email, $pass){
 
@@ -51,5 +69,18 @@ class Users extends Model {
 	public function createJwt(){
 		$jwt = new Jwt();
 		return $jwt->create(array('id_user' => $this->id_user));
+	}
+
+	private function emailExists($email){
+		$sql = 'SELECT id FROM users WHERE email = :email';
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(':email', $email);
+		$sql->execute();
+
+		if($sql->rowCount() > 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
